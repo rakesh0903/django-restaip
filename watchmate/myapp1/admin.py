39c6c7f849bb2bcf06db6c4fb5b4d5dd9a71ea34
Rakesh Sangare <rakeshsangare93@gmail.com>
2023-08-20 13:16:35 +0530

    commit:9

--- watchmate/myapp1/admin.py
+++ watchmate/myapp1/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-
+from myapp1.models import Movie
 
 # Register your models here.
+admin.site.register(Movie)
--- watchmate/myapp1/models.py
+++ watchmate/myapp1/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Movie(models.Model):
+
+    name= models.CharField(max_length=20)
+    descriptios=models.CharField(max_length=200)
+    active=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
 
