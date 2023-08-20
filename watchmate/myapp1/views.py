ff0b6975cefc2915f06f12f12d6771fa0317514f
Rakesh Sangare <rakeshsangare93@gmail.com>
2023-08-20 16:32:03 +0530

    commit:11

--- watchmate/myapp1/views.py
+++ watchmate/myapp1/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from myapp1.models import Movie
 
 # Create your views here.
 
 #---------------------------------------
+# def movie_list(request):
+#     movies = Movie.objects.all()
+
+#     return HttpResponse(movies.values())
+
+#---------------------------------------
+
 def movie_list(request):
-    movies = Movie.objects.all()
+    movies = Movie.objects.all().values()
 
-    return HttpResponse(movies.values())
-
+    data ={
+        'movies':list(movies)
+    }
+    
+    return JsonResponse(data)
