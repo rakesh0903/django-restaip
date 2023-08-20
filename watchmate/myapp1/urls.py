820abb92dbe998eec7af03b65fef131301854f92
Rakesh Sangare <rakeshsangare93@gmail.com>
2023-08-20 17:00:44 +0530

    commit:12

--- watchmate/myapp1/urls.py
+++ watchmate/myapp1/urls.py
@@ -3,5 +3,6 @@ from myapp1 import views
 
 urlpatterns =[
     path('list/',views.movie_list,name='movie_list'),
-
+    path('<int:pk>/',views.movie_details,name='movie_details'),
+    
 ]
--- watchmate/myapp1/views.py
+++ watchmate/myapp1/views.py
@@ -13,10 +13,24 @@ from myapp1.models import Movie
 #---------------------------------------
 
 def movie_list(request):
-    movies = Movie.objects.all().values()
+    movies = Movie.objects.all().values() 
 
     data ={
         'movies':list(movies)
     }
     
     return JsonResponse(data)
+
+#---------------------------------------
+
+def movie_details(request,pk):
+    movie = Movie.objects.get(pk=pk)
+
+    data={
+        'movie':movie.name,
+        'description':movie.descriptios,
+        'active':movie.active
+    }
+
+    return JsonResponse(data)
+    
